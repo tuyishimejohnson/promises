5ade004dae634e4fdd63fb0dcb2ee09dce051ea2
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-24 15:57:57 +0200

    Post data using fetch API

--- PostData/postData.js
+++ PostData/postData.js
@@ -1,12 +1,11 @@
 
 const myform = document.getElementById("form")
 
-
 myform.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    const name = document.getElementById("name").value
-    const mybody = document.getElementById("bd").value
+    const name = document.getElementById("myname").value
+    const mybody = document.getElementById("mybody").value
     
     
     const data = {
@@ -17,11 +16,9 @@ myform.addEventListener("submit", (e) => {
     fetch("https://jsonplaceholder.typicode.com/posts", {
         method: "POST",
         headers: {
-            "Content-type": "Application/json",
+            "Content-Type": "application/json",
         },
-
         body: JSON.stringify(data)
-
     })
 
     .then(response => response.json())
@@ -30,20 +27,3 @@ myform.addEventListener("submit", (e) => {
 
     
 })
-
-
-
-/* fetch('https://example.com/api/data', {
-    method: 'POST', // Specify the method
-    headers: {
-      'Content-Type': 'application/json', // Set the content type to application/json
-    },
-    body: JSON.stringify({
-      key1: 'value1',
-      key2: 'value2',
-    }), // Convert the data to a JSON string
-  })
-  .then(response => response.json()) // Parse the JSON response
-  .then(data => console.log(data)) // Handle the parsed data
-  .catch((error) => console.error('Error:', error));
- */
