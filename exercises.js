b2f707720a62e4a7b558d348ae201c53a0861214
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-26 22:53:11 +0200

    Callback to promise

--- exercises.js
+++ exercises.js
@@ -1,6 +1,6 @@
 /* 1. Write a JavaScript function that takes a callback and invokes it after a delay of 2 second. */
 
-function hello(callback) {
+/* function hello(callback) {
     callback()
 }
 
@@ -11,3 +11,62 @@ function timeOut() {
 }
 
 hello(timeOut)
+ */
+/* 2. Write a JavaScript program that converts a callback-based function to a Promise-based function. */
+
+/* function callback(cb) {
+    setTimeout(() => {
+        const error = false
+        if(error) {
+            cb(undefined, "Error occured")
+        }
+        else {
+            cb("Successful", undefined)
+        }
+    }, 1000);
+    
+}
+
+
+const promiseBased = () => {
+    return new Promise((resolve, reject) => {
+        callback((err, data) => {
+            if(data) {
+                resolve(data)
+            } else {
+                reject(err)
+            }
+        })
+    })
+}
+
+promiseBased().then(value => console.log(value))
+              .catch(error => console.log("error " +  error))
+ */
+
+
+function callback(cb) {
+    setTimeout(() => {
+        const error = false;
+        if (error) {
+            cb("An error occurred", undefined);
+        } else {
+            cb(undefined, "Success");
+        }
+    }, 1000);
+}
+
+const promiseBased = () => {
+    return new Promise((resolve, reject) => {
+        callback((err, data) => {
+            if (data) {
+                resolve(data);
+            } else {
+                reject(err);
+            }
+        });
+    });
+}
+
+promiseBased().then(value => console.log(value))
+              .catch(error => console.log("error " +  error));
