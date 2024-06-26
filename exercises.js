b3ad303739ff8acce2889450086cdee608dd4a24
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-26 23:30:50 +0200

    Return promise

--- exercises.js
+++ exercises.js
@@ -1,6 +1,6 @@
 /* 1. Write a JavaScript function that takes a callback and invokes it after a delay of 2 second. */
 
-/* function hello(callback) {
+function hello(callback) {
     callback()
 }
 
@@ -11,39 +11,8 @@ function timeOut() {
 }
 
 hello(timeOut)
- */
+
 /* 2. Write a JavaScript program that converts a callback-based function to a Promise-based function. */
-
-/* function callback(cb) {
-    setTimeout(() => {
-        const error = false
-        if(error) {
-            cb(undefined, "Error occured")
-        }
-        else {
-            cb("Successful", undefined)
-        }
-    }, 1000);
-    
-}
-
-
-const promiseBased = () => {
-    return new Promise((resolve, reject) => {
-        callback((err, data) => {
-            if(data) {
-                resolve(data)
-            } else {
-                reject(err)
-            }
-        })
-    })
-}
-
-promiseBased().then(value => console.log(value))
-              .catch(error => console.log("error " +  error))
- */
-
 
 function callback(cb) {
     setTimeout(() => {
@@ -70,3 +39,43 @@ const promiseBased = () => {
 
 promiseBased().then(value => console.log(value))
               .catch(error => console.log("error " +  error));
+
+/* 3. Write a JavaScript a function that makes an HTTP GET request and returns a Promise that resolves with the response data. */
+
+
+
+const url = "https://jsonplaceholder.typicode.com/posts"
+async function getResult() { 
+
+    try {
+        const result = await fetch(url)
+        const mydata = await result.json()
+        console.log(mydata)
+
+    } catch (error) {
+        console.log("Error is " + error)
+    }
+    
+        
+}
+
+getResult()
+
+// without async await 
+
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error("Failed to fetch!")
+            }
+
+            return response.json()
+        }).then(value => resolve(value))
+        .catch(error => reject(error))
+    })
+}
+
+getData("https://jsonplaceholder.typicode.com/posts").then(res => console.log(res))
+          .catch(error => console.log(error))
