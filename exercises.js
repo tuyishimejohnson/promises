8afb2d0f8f7ce3a455c82d82bd9ee42cc643fd04
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-06-27 11:23:50 +0200

    Vehicle manufacturers

--- exercises.js
+++ exercises.js
@@ -97,14 +97,18 @@ getData("https://jsonplaceholder.typicode.com/posts").then(res => console.log(re
 const urls = [url1, url2, url3]
  */
 
-/* 2. Use the `async/await` syntax to fetch vehicle manufacturers’ data from the given API endpoint. After fetching, filter the results and log to the console only manufacturers whose `Country` is equal to `"UNITED STATES (USA)"` */
+/* 5. Use the `async/await` syntax to fetch vehicle manufacturers’ data from the given API endpoint. After fetching, filter the results and log to the console only manufacturers whose `Country` is equal to `"UNITED STATES (USA)"` */
 
 async function vehicleManufactures(url) {
     const res = await fetch(url)
     const data = await res.json()
     let dataRes = data.Results
-    console.log(dataRes)
-
+    
+    dataRes.forEach(ele => {
+        if(ele.Country === "UNITED STATES (USA)") {
+            console.log(ele)
+        }
+    });
     
 }
 
