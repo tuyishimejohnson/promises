cece2362af508c62afe1e3b06d46b5bc79b47b5d
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-01 20:42:51 +0200

    Get fast posts

--- exercise7.js
+++ exercise7.js
@@ -1,44 +1,25 @@
-const fetchArray = [
+
+const urls = [
     "https://dummyjson.com/posts",
     "https://jsonplaceholder.typicode.com/posts",
-    "https://this-may-not-exist.com/posts"
+    "https://catfact.ninja/fact"
     
 ]
 
 const getFastPosts = async () => {
-    try {
-        const res = await Promise.race(fetchArray.map(url => fetch(url)))
-        const data = await res.json()
-        console.log(data)
-    } catch (error) {
-        console.error("Promise failed")
-    }
-    
+
+    const fetchData = urls.map(url => fetch(url).then(response => {
+        if(!response.ok) {
+            throw new Error("Fetch failed!")
+        }
+
+        return response.json()
+    }))
+
+    const data = await Promise.race(fetchData)
+    return data
 }
 
-getFastPosts()
+getFastPosts().then(result => console.log(result))
+.catch(error => console.log(error))
 
-/* getFastPosts().then(value => console.log(value))
-.catch(error => console.error(error)) */
-
-
-/* let a = 1
-console.log(a)
-setTimeout(() => {
-  console.log('Timeout: ', a)
-  a =2
-}, 0)
-
-setImmediate(() => {
-  console.log('immediate:', a)
-})
-
-let p = new Promise((resolve, reject)=> {
-   console.log('Am within a promise')
-  resolve()
-  a = 3
-})
-
-p.then(()=> console.log('am in promise:', a))
-
-console.log('Am out in free space', a) */
