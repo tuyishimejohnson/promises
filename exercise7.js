90ed2ee212d0641352ceabe4830b35914296e83f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-01 20:43:28 +0200

    Get fast posts

--- exercise7.js
+++ exercise7.js
@@ -1,3 +1,16 @@
+/* 
+
+Imagine you are developing a real-time news application, and you need to fetch posts from three different sources to provide users with the latest updates. 
+
+The API endpoints for getting posts are:
+
+- https://dummyjson.com/posts
+- https://this-may-not-exist.com/posts
+- https://jsonplaceholder.typicode.com/posts
+
+To ensure a seamless user experience, you are supposed to create a function called getFastPosts that fetches posts from these endpoints simultaneously (concurrently) and only presents data from the source that responds the quickest, while ignoring slower or potentially unreliable sources.
+
+*/
 
 const urls = [
     "https://dummyjson.com/posts",
